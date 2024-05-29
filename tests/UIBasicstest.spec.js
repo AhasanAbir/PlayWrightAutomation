389efc675d9bfc1a6c91f0744ca19ce30aef61f2
AhasanAbir <ahasanabir97@gmail.com>
2024-05-29 12:50:08 +0600

    29 May

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -8,10 +8,19 @@ test('Browser Context Playwright test' , async ({browser}) =>
         await page.goto("http:google.com");
 });
 
-test.only('page Playwright test' , async ({page}) =>
+test('page Playwright test' , async ({page}) =>
 {
        await page.goto("http:google.com");
        //get the title
        console.log(await page.title());
        await expect(page).toHaveTitle("Google")
+});
+test.only('UI Controls', async ({page}) =>
+{
+        await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+        const userName = page.locator('#username');
+        const signIn = page.locator('#signInBtn');
+        const dropdown = page.locator("select.form-control")
+        await dropdown.selectOption("consult");
+        await page.pause();
 });
