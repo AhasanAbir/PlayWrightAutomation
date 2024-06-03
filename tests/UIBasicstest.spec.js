0f157bd4d050b792666852784c48cd582e9d76dd
AhasanAbir <ahasanabir97@gmail.com>
2024-06-03 15:42:13 +0600

    03 June

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,17 +1,26 @@
 const {test, expect} = require('@playwright/test');
 
 
-test('Browser Context Playwright test' , async ({browser}) =>
+test.only('Browser Context Playwright test' , async ({browser}) =>
 {
         const context = await browser.newContext();
         const page = await context.newPage();
+        const userName = page.locator('#username');
+        const signIn = page.locator('#signInBtn');
         await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
         console.log(await page.title());
         //CSS. Xpath
-        await page.locator('#username').type("rahulshetty");
+        await userName.type("rahulshetty");
         await page.locator("[type='password']").type("learning");
-        await page.locator("#signInBtn").click();
+        await signIn.click();
         console.log(await page.locator("[style*='block']").textContent());
+        //type -fill
+        await userName.fill("");
+        await userName.fill("rahulshettyacademy")
+        await signIn.click();
+        console.log(await page.locator (".card-body a").first().textContent())
+        console.log(await page.locator (".card-body a").nth(1).textContent())
+
 });
 
 
