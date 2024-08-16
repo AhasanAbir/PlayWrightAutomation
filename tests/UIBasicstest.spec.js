69b360be5155d545b0141852f0e1c09f71d0e735
AhasanAbir <ahasanabir97@gmail.com>
2024-08-16 15:34:36 +0600

    16th August

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -33,6 +33,7 @@ test.only('UI Controls', async ({page}) =>
         await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
         const userName = page.locator('#username');
         const signIn = page.locator('#signInBtn');
+        const documentLink = page.locator("[href*='documents-request']");
         const dropdown = page.locator("select.form-control")
         await dropdown.selectOption("consult");
         await page.locator(".radiotextsty").last().click();
@@ -43,4 +44,20 @@ test.only('UI Controls', async ({page}) =>
         await expect(page.locator("#terms")).toBeChecked();
         await page.locator("#terms").uncheck();  
         expect (await page.locator("#terms").isChecked()).toBeFalsy();
+        await expect(documentLink).toHaveAttribute("class", "blinkingText");
+});
+
+test.only ('Child windows handle', async ({page})=>
+{
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+        const documentLink = page.locator("[href*='documents-request']");
+        
+        const newPage = await Promise.all([
+                context.waitForEvent('page');
+                documentLink.click();
+        ])
+        
+        //git test
 });
